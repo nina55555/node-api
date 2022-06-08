f79ad7ab107aeb4799639fffbd3e02015a85ce62
Nina <morville5audrey@gmail.com>
2022-06-08 21:39:49 +0200

    add data api and crud method

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+//appel des paquets necessaires:
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+/* Si erreur d'update:
+const mongoose = require('mongoose');
+
+mongoose.set('useFindAndModify', false);
+*/
+
+//ou mettre après app
+require ('./models/dbConfig');
+
+const postsRoutes = require('./routes/postsController');
+const bodyParser = require('body-parser');
+
+//middlewares
+app.use(bodyParser.json());
+app.use(cors());
+/* si on veut restreindre a une adress en particulier
+app.use(cors({origin: 'http://localhost:5950/posts'}));
+*/
+app.use('/posts', postsRoutes);
+
+app.listen(5950, () => console.log('server connecté merveilleusement sur port: 5950'));
---
+++ routes/postsController.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const ObjectID = require('mongoose').Types.ObjectId;
+
+const { PostsModel } = require('../models/postsModel');
+
+router.get('/', (req, res) => {
+    PostsModel.find((err, docs) => {
+        if(!err)res.send(docs);
+        else console.log('erreur de recuperation des données:'+err);
+    })
+});
+
+router.post('/', (req, res) => {
+    //console.log(req.body);
+    const newRecord = new PostsModel({
+        author: req.body.author,
+        message: req.body.message
+    });
+
+    newRecord.save((err, docs) => {
+        if(!err)res.send(docs);
+        else console.log('erreur de creation de données:'+err);
+    })
+});
+
+//Pour modifier les données
+router.put('/:id', (req, res) => {
+    if(!ObjectID.isValid(req.params.id))
+    return res.status(400).send('ID inconnu :' + req.params.id)
+
+    const updateRecord = {
+        author: req.body.author,
+        message: req.body.message
+    };
+
+    PostsModel.findByIdAndUpdate(
+        req.params.id,
+        { $set: updateRecord},
+        { new: true},
+        (err, docs) => {
+            if(!err)res.send(docs);
+            else console.log('erreur avec notre update :' + err);
+        }
+    )
+});
+
+router.delete('/:id', (req, res) => {
+    if(!ObjectID.isValid(req.params.id))
+    return res.status(400).send('ID inconnu :' + req.params.id)
+
+    PostsModel.findByIdAndRemove(
+        req.params.id,
+        (err, docs) => {
+            if(!err)res.send(docs);
+            else console.log('erreur de suppression des données :' + err);
+        }
+    )
+});
+
+module.exports = router;
